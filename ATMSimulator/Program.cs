5328336da32a6b734bf35575b6f8d37638f0a070
Jeff Roden <roden80@gmail.com>
2024-09-18 20:58:24 -0500

    start main menu

--- ATMSimulator/Program.cs
+++ ATMSimulator/Program.cs
@@ -11,17 +11,44 @@ class Program
         UserInterface.Start();
 
         User? user = null;
-        
         while (user == null)
         {
             user = Login.LoginUser(user);
         }
+        
+        MainMenu(user);
+    }
+
+    private static void MainMenu(User user)
+    {
         UserInterface.DrawOptions();
-        DataAccess data = new();
-        // data.GetUser(accountNumber: 123123);
-        while (true)
+
+        int selectedChoice = 0;
+        while (selectedChoice != 6)
         {
-            
+            //todo get input choice
+            // selectedChoice = gatherInput
+            switch (selectedChoice)
+            {
+                case 1:
+                    //balance
+                    break;
+                case 2:
+                    //Withdraw
+                    break;
+                case 3:
+                    //Transfer
+                    break;
+                case 4:
+                    //Fast Cash
+                    break;
+                case 5:
+                    //Transactions
+                    break;
+                case 6:
+                    //Logout
+                    break;
+            }
         }
     }
 }
